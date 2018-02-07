bf53427aef8be1173e18749350ad0433108b6aab
saibot777 <stefantrajkovic777@gmail.com>
2018-02-07 11:49:08 +0100

    initial commit...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MaterialModule} from "./material.module";
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { LayoutComponent } from './layout/layout.component';
+import { CurrentTrainingComponent } from './layout/current-training/current-training.component';
+import { NewTrainingComponent } from './layout/new-training/new-training.component';
+import { PastTrainingComponent } from './layout/past-training/past-training.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    LayoutComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
