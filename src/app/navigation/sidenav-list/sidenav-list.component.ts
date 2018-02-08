43b935389d62fd915de3f696999ce2925bf0256c
saibot777 <stefantrajkovic777@gmail.com>
2018-02-08 21:27:34 +0100

    auth implemented in sidenav

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,20 +1,31 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
+import {AuthService} from "../../core/services/auth.service";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
   @Output() onCloseLink = new EventEmitter<void>();
+  isAuth: boolean;
+  authSubscrioption: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authSubscrioption = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
   }
 
   public onClose() {
     this.onCloseLink.emit();
+  }
+
+  ngOnDestroy() {
+    this.authSubscrioption.unsubscribe();
   }
 
 }
