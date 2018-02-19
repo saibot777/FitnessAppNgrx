19adc859b9fe41c5561671180656246fb57a8100
saibot777 <stefantrajkovic777@gmail.com>
2018-02-19 15:58:01 +0100

    trainingReducer logic and actions done

--- src/app/store/app.actions.ts
+++ src/app/store/app.actions.ts
@@ -1,10 +1,16 @@
 import {Action} from "@ngrx/store";
+import {Exercise} from "../training/exercise.model";
 
 export const START_LOADING = '[UI] Start Loading';
 export const STOP_LOADING = '[UI] Stop Loading';
 
 export const SET_AUTH = '[Auth] Set Authentication';
 export const UNSET_AUTH = '[UI] UnSet Authentication';
+
+export const SET_AVAILABLE_TRAININGS = '[Training] Set Available Trainings';
+export const SET_FINISHED_TRAININGS = '[Training] Set Finished Trainings';
+export const START_TRAINING = '[Training] Start Training';
+export const STOP_TRAINING = '[Training] Stop Training';
 
 export class StartLoading implements Action {
   readonly type = START_LOADING;
@@ -22,8 +28,34 @@ export class UnsetAuth implements Action {
   readonly type = UNSET_AUTH;
 }
 
+export class SetAvailableTrainings implements Action {
+  readonly type = SET_AVAILABLE_TRAININGS;
+
+  constructor(public payload: Exercise[]) {}
+}
+
+export class SetFinishedTrainings implements Action {
+  readonly type = SET_FINISHED_TRAININGS;
+
+  constructor(public payload: Exercise[]) {}
+}
+
+export class StartTraining implements Action {
+  readonly type = START_TRAINING;
+
+  constructor(public payload: Exercise) {}
+}
+
+export class StopTraining implements Action {
+  readonly type = STOP_TRAINING;
+}
+
 export type AppActions =
-  StartLoading |
-  StopLoading |
-  SetAuth |
-  UnsetAuth;
+  StartLoading
+  | StopLoading
+  | SetAuth
+  | UnsetAuth
+  | SetAvailableTrainings
+  | SetFinishedTrainings
+  | StartTraining
+  | StopTraining;
--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { StoreModule } from '@ngrx/store';
 
 import { TrainingComponent } from './training.component';
 import { CurrentTrainingComponent } from './current-training/current-training.component';
 import { NewTrainingComponent } from './new-training/new-training.component';
 import { PastTrainingsComponent } from './past-trainings/past-trainings.component';
 import { StopTrainingComponent } from './current-training/stop-training.component';
-import { MaterialModule } from '../material.module';
 import { SharedModule } from '../shared/shared.module';
 import { TrainingRoutingModule } from './training-routing.module';
+import { trainingReducer } from "./store/training.reducer";
 
 @NgModule({
   declarations: [
@@ -18,11 +16,13 @@ import { TrainingRoutingModule } from './training-routing.module';
     CurrentTrainingComponent,
     NewTrainingComponent,
     PastTrainingsComponent,
-    StopTrainingComponent
+    StopTrainingComponent,
   ],
   imports: [
     SharedModule,
-    TrainingRoutingModule
+    TrainingRoutingModule,
+    StoreModule.forFeature('training', trainingReducer)
+
   ],
   entryComponents: [StopTrainingComponent]
 })
